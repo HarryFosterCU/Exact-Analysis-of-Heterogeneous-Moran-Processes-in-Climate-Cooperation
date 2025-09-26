a8675522f1a055bb47db5deab96407e625f422a5
Vince Knight <vince@vknight.org>
2025-09-26 12:20:38 +0100

    Write functionality to generate state space
    Also added a few edge case tests.

--- src/main.py
+++ src/main.py
@@ -5,3 +5,18 @@ This corresponds to the model described in `main.tex`
 
 TODO: Add mathematical details here.
 """
+import itertools
+
+def get_state_space(N, k):
+    """
+    Return state space for... TODO
+
+    Parameters:
+    -----------
+    TODO
+
+    Returns:
+    --------
+    TODO
+    """
+    return list(itertools.product(range(k), repeat=N))
--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,6 @@
 import main
 
-def test_generate_state_space():
+def test_generate_state_space_for_N_eq_3_and_k_eq_2():
     """
     Given a value of $N$: the number of individuals and a value of $k$: the
     number of types generate $S = [1, ..., k] ^ N$.
@@ -20,4 +20,38 @@ def test_generate_state_space():
         (1, 1, 1),
     ]
     obtained_state_space = main.get_state_space(N=N, k=k)
-    assert expected_state_space == obtained_state_space
+    assert sorted(expected_state_space) == sorted(obtained_state_space)
+
+
+def test_generate_state_space_for_N_eq_3_and_k_eq_1():
+    """
+    Given a value of $N$: the number of individuals and a value of $k$: the
+    number of types generate $S = [1, ..., k] ^ N$.
+
+    This tests this for N = 3, k = 1.
+    """
+    k = 1
+    N = 3
+    expected_state_space = [
+        (0, 0, 0),
+    ]
+    obtained_state_space = main.get_state_space(N=N, k=k)
+    assert sorted(expected_state_space) == sorted(obtained_state_space)
+
+
+def test_generate_state_space_for_N_eq_1_and_k_eq_3():
+    """
+    Given a value of $N$: the number of individuals and a value of $k$: the
+    number of types generate $S = [1, ..., k] ^ N$.
+
+    This tests this for N = 1, k = 3.
+    """
+    k = 3
+    N = 1
+    expected_state_space = [
+        (0,),
+        (1,),
+        (2,),
+    ]
+    obtained_state_space = main.get_state_space(N=N, k=k)
+    assert sorted(expected_state_space) == sorted(obtained_state_space)

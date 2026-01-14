4dce249a897dca15d0a9620c41c0041d06aed4fe
Harry Foster <Fosterh3@cardiff.ac.uk>
2026-01-14 13:44:44 +0000

    Run black on benchmarks

--- src/benchmarks/test_approximate_steady_state.py
+++ src/benchmarks/test_approximate_steady_state.py
@@ -18,31 +18,49 @@ step_size_prob = 2
 
 def generate_stochastic_2_valency_cycle_matrix(n, prob_prior):
     I = np.eye(n)
-    return prob_prior * (np.roll(I, -1, axis=1)) + (1 - prob_prior) * (np.roll(I, 1, axis=1))
+    return prob_prior * (np.roll(I, -1, axis=1)) + (1 - prob_prior) * (
+        np.roll(I, 1, axis=1)
+    )
 
 
 def generate_full_uniform_matrix(n):
     return np.full((n, n), 1 / n)
 
+
 def test_generate_stochastic_2_valency_cycle_matrix():
     """Tests that generate_stochastic_2_valency_cycle_matrix is correct for the
     case of both deterministic cycle and stochastic valency"""
 
-    deterministic_matrix = np.array([[0,1,0],[0,0,1],[1,0,0]])
-    np.testing.assert_array_equal(deterministic_matrix, generate_stochastic_2_valency_cycle_matrix(n=3, prob_prior=0))
+    deterministic_matrix = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
+    np.testing.assert_array_equal(
+        deterministic_matrix,
+        generate_stochastic_2_valency_cycle_matrix(n=3, prob_prior=0),
+    )
 
-    stochastic_matrix = np.array([[0, 0.3, 0, 0.7], [0.7, 0, 0.3, 0], [0, 0.7, 0, 0.3], [0.3, 0, 0.7, 0]])
-    np.testing.assert_array_almost_equal(stochastic_matrix, generate_stochastic_2_valency_cycle_matrix(n=4, prob_prior=0.7))
+    stochastic_matrix = np.array(
+        [[0, 0.3, 0, 0.7], [0.7, 0, 0.3, 0], [0, 0.7, 0, 0.3], [0.3, 0, 0.7, 0]]
+    )
+    np.testing.assert_array_almost_equal(
+        stochastic_matrix,
+        generate_stochastic_2_valency_cycle_matrix(n=4, prob_prior=0.7),
+    )
+
 
 def test_generate_full_uniform_matrix():
     """Tests that generate_full_uniform_matrix is correct"""
 
-    uniform_matrix = np.array([[1/4,1/4,1/4, 1/4], [1/4,1/4,1/4, 1/4], [1/4,1/4,1/4, 1/4], [1/4,1/4,1/4, 1/4]])
+    uniform_matrix = np.array(
+        [
+            [1 / 4, 1 / 4, 1 / 4, 1 / 4],
+            [1 / 4, 1 / 4, 1 / 4, 1 / 4],
+            [1 / 4, 1 / 4, 1 / 4, 1 / 4],
+            [1 / 4, 1 / 4, 1 / 4, 1 / 4],
+        ]
+    )
     np.testing.assert_array_equal(uniform_matrix, generate_full_uniform_matrix(4))
 
 
-@pytest.mark.parametrize("n",
-    range(2, n_range, step_size_n))
+@pytest.mark.parametrize("n", range(2, n_range, step_size_n))
 def test_approximate_steady_state_for_deterministic_cycle_matrix(n, benchmark):
     """Benchmarks approximate_steady_state for the deterministic cycle
     matrix"""
@@ -51,12 +69,17 @@ def test_approximate_steady_state_for_deterministic_cycle_matrix(n, benchmark):
     benchmark(main.approximate_steady_state, transition_matrix)
 
 
-@pytest.mark.parametrize("n, prob_prior", [
-    (n, p)
-    for n in range(2, n_range, step_size_n)
-    for p in [1 / (i) for i in range(1, prob_range + 1, step_size_prob)]
-])
-def test_approximate_steady_state_for_stochastic_2_valency_cycle_matrix(n,prob_prior, benchmark):
+@pytest.mark.parametrize(
+    "n, prob_prior",
+    [
+        (n, p)
+        for n in range(2, n_range, step_size_n)
+        for p in [1 / (i) for i in range(1, prob_range + 1, step_size_prob)]
+    ],
+)
+def test_approximate_steady_state_for_stochastic_2_valency_cycle_matrix(
+    n, prob_prior, benchmark
+):
     """Benchmarks approximate_steady_state for the stochastic 2-valency cycle
     matrix"""
 

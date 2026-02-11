9db17ba581b1943d5b6439ed86c1e5a18fd7214b
Harry Foster <127303006+HarryFosterCU@users.noreply.github.com>
2026-02-11 10:59:10 +0000

    Fix epsilon assets (#97)
    * Add updated epsilon and beta changes to assets
    
    * update general_2_player_heterogeneous graph
    
    * implemented comments
    
    * run black
    
    * change import file names

--- assets/tex/instrospection_steady_state_2_by_2/main.py
+++ assets/tex/instrospection_steady_state_2_by_2/main.py
@@ -8,22 +8,7 @@ root_path = (file_path / "../../../../").resolve()
 
 sys.path.append(str(root_path))
 import src.main as main
-
-def fitness_function_2_by_2(state, **kwargs):
-
-    f = sym.Function("f")
-    if (state == np.array([0, 0])).all():
-        state_symbol = sym.Symbol("a")
-    elif (state == np.array([0, 1])).all():
-        state_symbol = sym.Symbol("b")
-    elif (state == np.array([1, 0])).all():
-        state_symbol = sym.Symbol("c")
-    elif (state == np.array([1, 1])).all():
-        state_symbol = sym.Symbol("d")
-
-    return np.array(
-        [sym.Function(f"f_{i+1}")(state_symbol) for i, j in enumerate(state)]
-    )
+import src.fitness_functions as fitness_functions
 
 r = sym.Symbol("r")
 epsilon = sym.Symbol("epsilon")
@@ -33,11 +18,11 @@ beta = sym.Symbol("beta")
 
 transition_matrix = main.generate_transition_matrix(
     state_space=state_space,
-    fitness_function=fitness_function_2_by_2,
+    fitness_function=fitness_functions.general_four_state_fitness_function,
     compute_transition_probability=main.compute_introspection_transition_probability,
-    selection_intensity=beta,
-    number_of_strategies=2
-    )
+    choice_intensity=beta,
+    number_of_strategies=2,
+)
 
 steady_state = main.calculate_steady_state(transition_matrix)
 
@@ -46,4 +31,4 @@ with open(
     file_path.parent / "main.tex",
     "w",
 ) as f:
-    f.write(sym.latex(sym.Matrix(general_heterogeneous_absorption_matrix_n_2)))
+    f.write(sym.latex(sym.Matrix(steady_state)))
